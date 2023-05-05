d86294257da0e89394f04a216a729cffb30378a7
TaeeunKim <xodms0309@naver.com>
2023-05-05 14:04:01 +0900

    0.0.6v

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader', 'ts-loader'],
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
   },
 
